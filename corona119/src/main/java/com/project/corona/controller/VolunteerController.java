3e24b15d95385828bde8d410b183de283098a826
pracgoott2 <pracgoott2@ruu.kr>
2020-05-06 17:58:22 +0900

    게시글 수정시 에러 픽스

--- corona119/src/main/java/com/project/corona/controller/VolunteerController.java
+++ corona119/src/main/java/com/project/corona/controller/VolunteerController.java
@@ -129,6 +129,7 @@ public class VolunteerController {
 			model.addAttribute("vImage", image);
 		} catch (Exception e) {
 		}
+		
 		try {
 			List<FileVO> file = volunteerService.findFileByBoardNo(boardNo);
 			model.addAttribute("vFile", file);
