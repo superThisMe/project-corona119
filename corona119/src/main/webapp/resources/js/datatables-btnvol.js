1419826a5b9fa599f8f394a87837dadbd39c080d
pracgoott2 <pracgoott2@ruu.kr>
2020-05-06 18:49:18 +0900

    로그인시 기능 빠뜨린 js 추가

--- corona119/src/main/webapp/resources/js/datatables-btnvol.js
+++ corona119/src/main/webapp/resources/js/datatables-btnvol.js
@@ -1,4 +1,4 @@
-
+$(function() {
 	var table = $('#dataTableVol').DataTable({
 		"stateSave" : true,
 		"pagingType" : "first_last_numbers",
@@ -25,3 +25,53 @@
 		]
 	});
 	$("#dataTableVol_info").css({"padding" : "0" , "margin" : "2px 0"});
+	
+	$('#seoulMap area').each(function(index, item){
+		$(item).on('mouseover', function(){
+			$('#seoul-map-image').attr('src', '/corona/resources/img/map/img_map_s' + (index + 1) + '.gif');	
+		});
+		$(item).on('click', function(){
+			$('#areaSearch').val($(this).attr('alt'));
+			filterColumn(3);
+		});
+	});
+	
+	$('#viewAllAreaBtn').on('click', function(){
+		$('#areaSearch').val("");
+		filterColumn(3);
+	});
+	
+	function filterColumn ( i ) {
+	    $('#dataTableVol').DataTable().column( i ).search(
+	        $('#areaSearch').val()
+	    ).draw();
+	}
+
+	$(document).on('change', '#confirmExt', function() {
+		
+		$.fn.dataTable.ext.search.push (
+			function( settings, data, dataIndex ) {
+				var exc = $('#confirmExtSearch').val();
+
+				if (exc != null) {
+					var vfw = data[4];
+					var ref = new RegExp(exc, 'g');
+				
+					if (vfw.match(ref)){	
+						return false;
+					}
+					return true;
+				}
+			}
+		);
+		
+		if($("#confirmExt").is(":checked")) {
+			$('#confirmExtSearch').val("종료");
+		} else {
+			$('#confirmExtSearch').val("dummyFinalProject");
+		}
+		table.draw();
+	});
+	
+
+});
